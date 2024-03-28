2f422a0c9421001119b021f332e3857e2f2feb29
Creston <barker.creston@gmail.com>
2024-03-28 11:11:35 -0500

    Reworked code and file system for new prototype, currently nonfunctional

--- main/main.ino
+++ main/main.ino
@@ -1,86 +1,89 @@
-#include <AccelStepper.h>
 #include <Adafruit_MotorShield.h>
 #include "Adafruit_seesaw.h"
+#include <Servo.h>
 
 Adafruit_seesaw ss;
-Adafruit_MotorShield AFMStop(0x60); // Default address, no jumpers
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-// Connect two steppers with 200 steps per revolution (1.8 degree)
-// to the top shield
-Adafruit_StepperMotor *myStepper1 = AFMStop.getStepper(200, 1);
-Adafruit_StepperMotor *myStepper2 = AFMStop.getStepper(200, 2);
+Servo servo1;
 
-// Wrappers for the first motor
-void forwardstep1() {
-  myStepper1->onestep(FORWARD, SINGLE);
-}
-void backwardstep1() {
-  myStepper1->onestep(BACKWARD, SINGLE);
-}
+int WETVAL = 950; //temp wetval
+int DRYVAL = 450; //temp dryval
+//value determinig if servo was put in open or closed state
+bool servo1open = false;
 
-// Wrappers for the second motor
-void forwardstep2() {
-  myStepper2->onestep(FORWARD, SINGLE);
-}
-void backwardstep2() {
-  myStepper2->onestep(BACKWARD, SINGLE);
-}
 
-// Create AccelStepper objects for the two steppers
-AccelStepper stepper1(forwardstep1, backwardstep1);
-AccelStepper stepper2(forwardstep2, backwardstep2);
-
-int wetval = 500; //temp wetval
-int dryval = 500; //temp dryval
 
 void setup() {
+  Serial.println("First Line of Code");
   Serial.begin(115200);
-  AFMStop.begin(); // Start the top shield
+  Serial.println("HELP");
 
-  stepper1.setMaxSpeed(100.0);
-  stepper1.setAcceleration(100.0);
-
-  stepper2.setMaxSpeed(100.0);
-  stepper2.setAcceleration(100.0);
   bool seesawinit = false;
-  while(!seesawinit)
-  if (!ss.begin(0x36)) {
-    Serial.println("ERROR! seesaw not found");
-    delay(2000);
-  } else {
-    Serial.print("seesaw started! version: ");
-    Serial.println(ss.getVersion(), HEX);
-    seesawinit = true;
+  while(!seesawinit) {
+    if (!ss.begin(0x36)) {
+      Serial.println("ERROR! seesaw not found");
+      delay(2000);
+    } else {
+      Serial.print("seesaw started! version: ");
+      Serial.println(ss.getVersion(), HEX);
+      seesawinit = true;
+    }
+  }
+  
+  bool afmsinit = false;
+  while(!afmsinit) {
+    if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+      Serial.println("Could not find Motor Shield. Check wiring.");
+      delay(2000);
+    } else { 
+      Serial.println("Motor Shield found.");
+      afmsinit = true;
+    }
+  }
+
+  // Attach a servo to pin #10
+  servo1.attach(10);
+
+  // Get initial cap value
+  uint16_t initread = ss.touchRead(0);
+
+  if(initread >= 500) { // Rotate to closed potiiton if above or equal to arbitrary
+    servo1.write(180);
+    delay(800);
+    servo1.write(90);
+    servo1open = false;
+  } else if (initread < 500) { // Rotate to open position if below arbitrary
+    servo1.write(0);
+    delay(800);
+    servo1.write(90);
+    servo1open = true;
   }
 }
 
-
 void loop() {
-
-  //wait to run
-  while (stepper1.distanceToGo() != 0 || stepper2.distanceToGo() != 0) {
-    stepper1.run();
-    stepper2.run();
-  }
- 
   //take capacitive value
   uint16_t capread = ss.touchRead(0);
+  
 
   // test for wet / dry conditions
-  if(capread > wetval) { // Rotate 90 degrees clockwise if wet 
-  stepper1.moveTo(45); // 200 steps for 90 degrees
-  stepper2.moveTo(45); // 200 steps for 90 degrees
-  } else if (capread < dryval) { // Rotate 90 degrees counterclockwise if dry
-  stepper1.moveTo(0); // -200 steps for 90 degrees (opposite direction)
-  stepper2.moveTo(0); // -200 steps for 90 degrees (opposite direction)
+  if((capread > WETVAL) && (servo1open == true)) { 
+  // Rotate to closed potiiton if wet and open
+    servo1.write(180);
+    delay(800);
+    servo1.write(90);
+  } else if ((capread < DRYVAL) && (servo1open == false)) { 
+    // Rotate to open position if dry and closed
+    servo1.write(0);
+    delay(800);
+    servo1.write(90);
   }
-
   
   float tempC = ss.getTemp();
 
   Serial.print("Temperature: "); Serial.print(tempC); Serial.println("*C");
   Serial.print("Capacitive: "); Serial.println(capread);
 
-    // Pause for 2 seconds
+  // Pause for 2 seconds
   delay(2000);
 }
